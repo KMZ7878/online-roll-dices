c226ac605eb04035904aca77ce83f0ea2461273f
KMZ7878 <marqueskaue771@gmail.com>
2024-05-29 19:39:12 -0300

    multy dices
    new dices selector

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+var qmoeda = document.getElementById('moeda');
+var option = selectElement.querySelector('option[value="1"]');
 
+
+
+
+
+
+
+
+
+
+
+
+
+let dadoAtual = null;
 function rolarDado(dado) {
     const resultado = Math.floor(Math.random() * dado) + 1;
-    document.getElementById("dadinho").innerHTML = resultado;
+    document.getElementById("D6").innerHTML = resultado;
 }
 
 document.getElementById("selector").addEventListener("click", function(event) {
     const target = event.target;
     if (target.tagName === "BUTTON") {
-        const dado = parseInt(target.getAttribute("data-dado"));
-        rolarDado(dado);
+        dadoAtual = parseInt(target.getAttribute("data-dado")); // Atualiza o dado atual
+        rolarDado(dadoAtual);
+    }
+});
+
+document.getElementById("rolar").addEventListener("click", function() {
+    if (dadoAtual !== null) {
+        rolarDado(dadoAtual); // Rola o dado salvo na variável global
+    } else {
+        alert("Selecione um dado primeiro!");
     }
 });
