b507cf9def3244efd7b3d2e738b3da172228e137
KMZ7878 <marqueskaue771@gmail.com>
2024-05-28 23:43:44 -0300

    otherdices
    adition of some new dices
    still not working perfectly

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function rolarDado(){
-    resultado = Math.floor(Math.random() * 6) + 1;
+
+function rolarDado(dado) {
+    const resultado = Math.floor(Math.random() * dado) + 1;
     document.getElementById("dadinho").innerHTML = resultado;
 }
+
+document.getElementById("selector").addEventListener("click", function(event) {
+    const target = event.target;
+    if (target.tagName === "BUTTON") {
+        const dado = parseInt(target.getAttribute("data-dado"));
+        rolarDado(dado);
+    }
+});
